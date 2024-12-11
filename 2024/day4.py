08b61f185653ac76e7cba6f012b2f7d25fbeada9
Christian Hellum Bye <chbye@berkeley.edu>
2024-12-10 17:33:46 -0800

    formatting

--- 2024/day4.py
+++ 2024/day4.py
@@ -6,11 +6,14 @@ fname = "day4.txt"
 with open(f"{folder}/{fname}") as f:
     data = f.readlines()
 
+
 def f(line, match="XMAS"):
     return line.count(match)
 
+
 def cnt(d):
     return sum(f(line) for line in d)
+
 
 def reverse(d):
     """
@@ -18,22 +21,25 @@ def reverse(d):
     """
     return [_d[::-1] for _d in d]
 
+
 def transpose(d):
     dt = []
     for i in range(len(d[0])):
         dt.append("".join([line[i] for line in d]))
     return dt
 
+
 def diag(d, pad):
     N = len(d)
     new_d = []
     for i, l in enumerate(d):
         if pad == "left":
-            row = (N-i) * "0" + l + i * "0"
+            row = (N - i) * "0" + l + i * "0"
         elif pad == "right":
-            row = i * "0" + l + (N-i) * "0"
+            row = i * "0" + l + (N - i) * "0"
         new_d.append(row)
     return transpose(new_d)
+
 
 fwd = data
 bwd = reverse(fwd)
@@ -49,18 +55,20 @@ for d in [fwd, bwd, dwn, up, dia_dr, dia_ul, dia_dl, dia_ur]:
     s += cnt(d)
 print(s)
 
+
 # part 2
 def cross_mas(d, line_num):
     if line_num == 0 or line_num == len(d) - 1:
         return 0
-    above, line, below = d[line_num-1:line_num+2]
+    above, line, below = d[line_num - 1 : line_num + 2]
     tot = 0
     for i, c in enumerate(line):
         if c == "A":
-            r = above[i-1] + c + below[i+1]
-            l = above[i+1] + c + below[i-1]
+            r = above[i - 1] + c + below[i + 1]
+            l = above[i + 1] + c + below[i - 1]
             if "MAS" in [r, r[::-1]] and "MAS" in [l, l[::-1]]:
                 tot += 1
     return tot
+
 
 print(sum(cross_mas(data, n) for n in range(len(data))))
--- 2024/day5.py
+++ 2024/day5.py
@@ -6,39 +6,45 @@ fname = "day5.txt"
 with open(f"{folder}/{fname}") as f:
     data = f.readlines()
 
+
 def parse_rule(rule):
     rule = rule.strip("\n").split("|")
     return int(rule[0]), int(rule[1])
 
+
 def parse_update(update):
     return [int(x) for x in update.strip("\n").split(",")]
 
+
 def ordered(update, rules):
-    for i in range(len(update)-1):
-        rev_pair = (update[i+1], update[i])
+    for i in range(len(update) - 1):
+        rev_pair = (update[i + 1], update[i])
         if rev_pair in rules:
             return False
     return True
 
+
 s_ix = data.index("\n")
 rules = [parse_rule(rule) for rule in data[:s_ix]]
-updates = [parse_update(update) for update in data[s_ix+1:]]
+updates = [parse_update(update) for update in data[s_ix + 1 :]]
 
 s = 0
 for up in updates:
     if ordered(up, rules):
-        s += up[int(len(up)//2)]
+        s += up[int(len(up) // 2)]
 print(s)
+
 
 def order(update_old, rules):
     update = update_old.copy()
     fixed = 0
-    for i in range(len(update)-1):
-        rev_pair = (update[i+1], update[i])
+    for i in range(len(update) - 1):
+        rev_pair = (update[i + 1], update[i])
         if rev_pair in rules:
-            update[i], update[i+1] = rev_pair   # flip
+            update[i], update[i + 1] = rev_pair  # flip
             fixed += 1
     return update, fixed
+
 
 s = 0
 for up in updates:
@@ -49,5 +55,5 @@ for up in updates:
         new_up, fixed = order(new_up, rules)
         its += 1
     if its > 1:
-        s += new_up[int(len(new_up)//2)]
+        s += new_up[int(len(new_up) // 2)]
 print(s)
