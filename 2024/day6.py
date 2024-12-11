65a28251a0171a0e1fe260171b1bba2b0f877875
Christian Hellum Bye <chbye@berkeley.edu>
2024-12-10 17:33:53 -0800

    pt2

--- 2024/day6.py
+++ 2024/day6.py
@@ -5,16 +5,20 @@ if TEST:
 else:
     path = f"inputs/{fname}"
 
-with open(path) as f:
-    data = [list(line.strip("\n")) for line in f.readlines()]
+
+def read_input(path):
+    with open(path) as f:
+        data = [list(line.strip("\n")) for line in f.readlines()]
+    return data
+
+
+DATA = read_input(path)
+
 
 class Guard:
-
-    pos = []
-    Npos = 0
-    d = 1j
-
-    def __init__(self, data=data):
+    def __init__(self, data=DATA):
+        self.pos = []
+        self.Npos = 0
         self.data = {}
         for y, line in enumerate(data[::-1]):  # y increases upwards
             for x, c in enumerate(line):
@@ -24,27 +28,62 @@ class Guard:
     def find_start(self):
         for k, v in self.data.items():
             if v == "^":
-                self.pos.append(k)
+                self.data[k] = "X"
+                self.pos.append((k, 1j))
                 self.Npos += 1
                 return
 
-    def move(self):
-        p = self.pos[-1] + self.d
-        if not p in self.data:
-            return False
-        c = self.data[p]
+    def move(self, part=1, verbose=False):
+        p = sum(self.pos[-1])
+        try:
+            c = self.data[p]
+        except KeyError:
+            return 0
         if c == ".":
             self.data[p] = "X"
-            self.pos.append(p)
+            self.pos.append((p, self.pos[-1][1]))
             self.Npos += 1
         elif c == "X":
-            self.pos.append(p)
+            cp = (p, self.pos[-1][1])
+            self.pos.append(cp)
+            if part == 2 and cp in self.pos[:-1]:
+                return 2
         elif c == "#":
-            self.d *= -1j
-        return True
+            pos = self.pos[-1]
+            cp = (pos[0], pos[1] * -1j)
+            self.pos.append(cp)
+            if part == 2 and cp in self.pos[:-1]:
+                return 2
+        return 1
 
+
+print("Part 1\n------")
 g = Guard()
-print(g.pos)
 while g.move():
-    print(g.pos[-1], g.Npos)
+    pass
 print(g.Npos)
+
+# pt 2
+print("Part 2\n------")
+N = len(DATA)
+M = len(DATA[0])
+n_obs = 0
+s = []
+for i in range(N):
+    for j in range(M):
+        if i % 10 == 0 and j == 0:
+            print(f"{i}/{N} {j}/{M}, {n_obs=}")
+        d = read_input(path)
+        if d[i][j] in ["#", "^"]:
+            continue
+        d[i][j] = "#"
+        g = Guard(data=d)
+        r = 1
+        while r == 1:
+            r = g.move(part=2)
+        if r == 2:
+            n_obs += 1
+            s.append((i, j))
+        del g
+        del d
+print(n_obs)
